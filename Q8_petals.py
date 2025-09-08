2eb6b00f2c6fbaaeb0e04997b99cc0e9b43e2df0
Ashiquejo <joashique@gmail.com>
2025-09-08 13:58:58 +0530

    Create Q8_petals.py

---
+++ Q8_petals.py
@@ -0,0 +1,21 @@
+import turtle
+def draw_petal(t, radius, arc_angle):
+    for _ in range(2):
+        t.circle(radius, arc_angle)
+        t.left(180 - arc_angle)
+n=int(input("Enter the no of peltals: "))        
+t = turtle.Turtle()
+t.pensize(2)
+t.speed(50)
+t.color("black")
+
+t.up()
+t.goto(0, 0)
+t.down()
+petals =n
+radius = 60
+arc_angle = 60
+for _ in range(petals):
+    draw_petal(t, radius, arc_angle)
+    t.left(360 / petals)
+turtle.done()
